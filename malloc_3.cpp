c4ca4c57ecd5a8ec7f3847919a8fa0975f09ca6a
or <orch81@gmail.com>
2023-07-01 16:47:44 +0300

    smalloc prat 3 - before tests

--- malloc_3.cpp
+++ malloc_3.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <iostream>
 #include <string.h>
+#include <stdio.h>
+#include <assert.h>
 using namespace std;
 
 #define MAX_ORDER 10
@@ -16,38 +18,27 @@ struct MallocMetadata {
     void* address;
 };
 
-MallocMetadata* free_array[MAX_ORDER+1];
+MallocMetadata free_array[MAX_ORDER+1];
+MallocMetadata arr[32];
+bool is_first_malloc = true;
 
 struct MallocMetadata dummy_head = {
         0, false, nullptr, nullptr,nullptr
 };
 
-bool is_first_malloc = true;
 
-struct MallocMetadata* find_block(size_t size){
 
-    struct MallocMetadata* block = &dummy_head;
-    block = block->next;
+//// functions declarations:
+struct MallocMetadata* find_block(size_t size);
+int find_order(size_t size_with_metadata);
+MallocMetadata* get_best_fit(int order, int* best_fit_order);
 
-    if(!block){
-        return nullptr;
-    }
 
-    while(block){
-        if(block->is_free && block->size >= size){
-            return block;
-        }
-        block = block->next;
-    }
-
-    return nullptr;
-
-}
 
 //// ---1---
 void* smalloc(size_t size){
 
-//    cout << "---1---" << endl;
+
     if(size == 0){
         return nullptr;
     }
@@ -56,7 +47,7 @@ void* smalloc(size_t size){
     }
 
     if(is_first_malloc){
-
+        is_first_malloc = false;
         // align allocation
         void* curr_program_break = sbrk(0);
         size_t reminder = (size_t)curr_program_break % (32*128*1024);
@@ -66,36 +57,169 @@ void* smalloc(size_t size){
 
         void* address_of_aligned_allocation = (void*)((size_t)address_of_initial_allocation + difference);
 
-        // save metadata to blocks
+        // save metadata to memory blocks
         for(int i = 0; i<32; i++){
-            struct MallocMetadata node = {
+            /*struct MallocMetadata node = {
                     128*1024, true, nullptr, nullptr,nullptr,
                     nullptr, (void*)((size_t)address_of_initial_allocation + i*128*1024)
             };
-            *(MallocMetadata*)((size_t)address_of_initial_allocation + i*128*1024) = node;
+*/
+            arr[i].size = 128*1024;
+            arr[i].is_free = true;
+            arr[i].next_free = nullptr;
+            arr[i].next = nullptr;
+            arr[i].prev_free = nullptr;
+            arr[i].prev = nullptr;
+            arr[i].address = (void*)((size_t)address_of_aligned_allocation + i*128*1024);
+            *(MallocMetadata*)((size_t)address_of_aligned_allocation + i*128*1024) = arr[i];
+        }
+
+        // initialize free_array:
+        for(int i = 0; i < MAX_ORDER+1; i++){
+            struct MallocMetadata dummy_node = {
+                    0, false, nullptr, nullptr,nullptr,
+                    nullptr, nullptr
+            };
+
+            free_array[i] = dummy_node;
+            free_array[i].size = 0;
+            free_array[i].is_free = false;
+            free_array[i].next_free = nullptr;
+            free_array[i].next = nullptr;
+            free_array[i].prev_free = nullptr;
+            free_array[i].prev = nullptr;
+            free_array[i].address = nullptr;
+
         }
 
         // set lists pointers
         for(int i = 0; i<32; i++){
-            MallocMetadata* curr_metadata_address =  (MallocMetadata*)((size_t)address_of_initial_allocation + i*128*1024);
-           if(i == 0){
-               free_array[MAX_ORDER]->next_free = curr_metadata_address;
-               curr_metadata_address->prev_free = free_array[MAX_ORDER];
-               curr_metadata_address->next = (MallocMetadata*)((size_t)address_of_initial_allocation + (i+1)*128*1024);
-               curr_metadata_address->prev = &dummy_head;
-           } else if(i == 31) {
-               curr_metadata_address->prev = (MallocMetadata*)((size_t)address_of_initial_allocation + (i-1)*128*1024);
-               curr_metadata_address->prev_free = (MallocMetadata*)((size_t)address_of_initial_allocation + (i-1)*128*1024);
-           } else {
-               curr_metadata_address->next = (MallocMetadata*)((size_t)address_of_initial_allocation + (i+1)*128*1024);
-               curr_metadata_address->next_free = (MallocMetadata*)((size_t)address_of_initial_allocation + (i+1)*128*1024);
-               curr_metadata_address->prev = (MallocMetadata*)((size_t)address_of_initial_allocation + (i-1)*128*1024);
-               curr_metadata_address->prev_free = (MallocMetadata*)((size_t)address_of_initial_allocation + (i-1)*128*1024);
-           }
+            MallocMetadata* curr_metadata_address =  (MallocMetadata*)((size_t)address_of_aligned_allocation + i*128*1024);
+            if(i == 0){
+                free_array[MAX_ORDER].next_free = curr_metadata_address;
+                curr_metadata_address->prev_free = &free_array[MAX_ORDER];
+                curr_metadata_address->next_free = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i+1)*128*1024);
+                curr_metadata_address->next = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i+1)*128*1024);
+                dummy_head.next = curr_metadata_address;
+                curr_metadata_address->prev = &dummy_head;
+            } else if(i == 31) {
+                curr_metadata_address->prev = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i-1)*128*1024);
+                curr_metadata_address->prev_free = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i-1)*128*1024);
+            } else {
+                curr_metadata_address->next = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i+1)*128*1024);
+                curr_metadata_address->next_free = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i+1)*128*1024);
+                curr_metadata_address->prev = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i-1)*128*1024);
+                curr_metadata_address->prev_free = (MallocMetadata*)((size_t)address_of_aligned_allocation + (i-1)*128*1024);
+            }
 
         }
     }
-    
+
+
+    size_t size_with_metadata = size + sizeof (MallocMetadata);
+
+    // find the order of wanted block
+    int order = find_order(size_with_metadata);
+    cout << "size entered: " << size << ", size with metadata: " << size_with_metadata;
+
+    if(order < 0){
+        // size bigger than 128KB
+        cout << " and size is bigger than 128KB!!!!!" << endl;
+        return nullptr;
+    }
+
+    cout << " and order is: " << order << endl;
+
+    int best_fit_order = 0;
+    MallocMetadata* best_fit_metadata = get_best_fit(order, &best_fit_order);
+    best_fit_metadata = free_array[best_fit_order].next_free;
+
+    if(!best_fit_metadata){
+        cout << "hereee" << endl;
+        return nullptr;
+    }
+    cout << "best_fit_order : " << best_fit_order << endl;
+
+
+    while(best_fit_order > order){
+        cout << "in loop" << endl;
+        // create new metadata
+
+        cout << "--1--" << endl;
+        struct MallocMetadata splitted_block_metadata = {
+                best_fit_metadata->size/2, true, nullptr, nullptr,nullptr,
+                nullptr, (void*)((size_t)best_fit_metadata->address + best_fit_metadata->size/2)
+        };
+        // update first half metadata
+        best_fit_metadata->size /= 2;
+
+        cout << "--2--" << endl;
+
+        // remove from linked list
+
+        free_array[best_fit_order].next_free = best_fit_metadata->next_free;
+        if(best_fit_metadata->next_free){
+            best_fit_metadata->next_free->prev_free = &free_array[best_fit_order];
+        }
+
+        cout << "--3--" << endl;
+
+        // add to new linked list
+        MallocMetadata* tmp = free_array[best_fit_order-1].next_free;
+        MallocMetadata* prev_tmp = &free_array[best_fit_order-1];
+        while(tmp){
+            if(tmp->address < best_fit_metadata->address){
+                prev_tmp = tmp;
+                tmp = tmp->next_free;
+                continue;
+            }
+            break;
+        }
+
+        cout << "--4--" << endl;
+        // if first half should be last node
+        if(!tmp){
+            tmp = prev_tmp;
+        } else {
+            tmp->next_free->prev_free = (MallocMetadata*)(splitted_block_metadata.address);
+        }
+        cout << "--5--" << endl;
+
+        splitted_block_metadata.next_free = tmp->next_free;
+        tmp->next_free = best_fit_metadata;
+        best_fit_metadata->prev_free = tmp;
+        best_fit_metadata->next_free = (MallocMetadata*)(splitted_block_metadata.address);
+        splitted_block_metadata.prev_free = best_fit_metadata;
+        cout << "--6--" << endl;
+
+
+        // add to original list
+        splitted_block_metadata.next = best_fit_metadata->next;
+        if(best_fit_metadata->next){
+            best_fit_metadata->next->prev = (MallocMetadata*)splitted_block_metadata.address;
+        }
+        best_fit_metadata->next = (MallocMetadata*)splitted_block_metadata.address;
+        splitted_block_metadata.prev = best_fit_metadata;
+
+
+        // save new metadata to memory
+        *(MallocMetadata*)(splitted_block_metadata.address) = splitted_block_metadata;
+        cout << "--7--" << endl;
+
+        best_fit_metadata = get_best_fit(order, &best_fit_order);
+        cout << "best_fit_metadata->order : " << best_fit_order << endl;
+    }
+
+
+    best_fit_metadata->is_free = false;
+
+    // remove best fit
+    free_array[best_fit_order].next_free = best_fit_metadata->next_free;
+    if(best_fit_metadata->next_free){
+        best_fit_metadata->next_free->prev_free = &free_array[best_fit_order];
+    }
+
+    return best_fit_metadata;
 }
 
 
@@ -223,15 +347,18 @@ size_t _num_allocated_blocks(){
     size_t counter = 0;
     struct MallocMetadata *block = &dummy_head;
 
+
     // start from first block - not from dummy
     block = block->next;
 
 //    cout << "--before while loop--" << endl;
     while(block){
+//        cout << "--in while loop--" << endl;
+//        cout << "block size: " << block->size << endl;
         counter++;
         block = block->next;
     }
-//    cout << "--after while loop--" << endl;
+
 
     return counter;
 
@@ -271,10 +398,96 @@ size_t _num_meta_data_bytes(){
 }
 
 
+//// utils
+struct MallocMetadata* find_block(size_t size){
+
+    struct MallocMetadata* block = &dummy_head;
+    block = block->next;
+
+    if(!block){
+        return nullptr;
+    }
+
+    while(block){
+        if(block->is_free && block->size >= size){
+            return block;
+        }
+        block = block->next;
+    }
+
+    return nullptr;
+}
+
+int find_order(size_t size_with_metadata){
+
+    for(int i = 0; i<MAX_ORDER+1 ; i++){
+        if(i == 0){
+            if((double)size_with_metadata <= pow(2,7)){
+                return 0;
+            }
+        } else {
+            if((double)size_with_metadata > pow(2,6+i) && (double)size_with_metadata <= pow(2, 7+i)){
+                return i;
+            }
+        }
+    }
+    // in case size bigger than 128KB:
+    return -1;
+}
+
+MallocMetadata* get_best_fit(int order, int* best_fit_order){
+
+    for(int i = order; i < MAX_ORDER+1; i++) {
+        if(free_array[i].next_free){
+            cout << "size is" << free_array[i].next_free->size << endl;
+            *best_fit_order = i;
+            return free_array[i].next_free;
+        }
+    }
+
+    return nullptr;
+}
+
+
+
+/*
+
 int main(){
 
     cout << "size = " << _size_meta_data() << endl;
+*/
+/*    smalloc(10);
+    smalloc(100);*//*
+
+    smalloc(1000);
+//    smalloc(1000000);
+    smalloc(10000);
+    smalloc(10000);
+//    smalloc(1000000);
+
+*/
+/*    assert(free_array[MAX_ORDER].next_free);
+    assert(!free_array[9].next_free->next_free);
+    assert(!free_array[8].next_free->next_free);
+    assert(!free_array[7].next_free);*//*
+
+
+    cout << "---finished allocation---" << endl;
+    int num_allocated = _num_allocated_blocks();
+    cout << "---num allocated blocks---" << endl;
+    int num_bytes = _num_allocated_bytes();
+    cout << "---num allocated bytes---" << endl;
+    int num_free = _num_free_blocks();
+    int num_free_bytes = _num_free_bytes();
+
+    cout << "num allocated blocks: " << num_allocated << endl;
+    cout << "num allocated bytes: " << num_bytes << endl;
+    cout << "num free blocks: " << num_free << endl;
+    cout << "num free bytes: " << num_free_bytes << endl;
+
+
 
     return 0;
 }
+*/
 
